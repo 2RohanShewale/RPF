7b5a5b3446ea7b18ad0054c4deb9d6f13f5f0b04
2RohanShewale <shewalerohan50@gmail.com>
2022-12-13 08:36:39 +0530

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int val = 7;
+            double val1 = 345.5;
+            Console.WriteLine("Write you name? ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Hello, World! "+val);
+            Console.WriteLine(username);
+        }
+    }
+}
